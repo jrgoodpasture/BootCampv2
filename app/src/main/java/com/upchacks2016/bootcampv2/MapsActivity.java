0c9f839c85999c81631b2328ce29cfeae90ff675
Jacob Goodpasture <jrgoodpasture@gmail.com>
2016-07-21 22:41:40 +0200

    added listview layouts

--- app/src/main/java/com/upchacks2016/bootcampv2/MapsActivity.java
+++ app/src/main/java/com/upchacks2016/bootcampv2/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(41.3896369,2.1172903);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Parc de Pedralbes"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
